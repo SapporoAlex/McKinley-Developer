3d3e6a71afc88ac96d0edfc409f5eb6bf9c66826
Alex_McKinley <alex.mckinley@oz-design.jp>
2026-09-03 22:28:02 +0900

    nc

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './i18n';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./i18n";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/React-Portfolio/">
+    <BrowserRouter basename="/McKinley-Developer/">
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
